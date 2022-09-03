3bb3b6086d85d28984af3189ecbeaf96c952eac9
alak-modk <112780802+alak-modk@users.noreply.github.com>
2022-09-04 01:56:16 +0530

    Add files via upload

---
+++ Config/db.js
@@ -0,0 +1,7 @@
+
+const mongoose = require("mongoose")
+//console.log(process.env.DB_URL)
+const connect =  async ()=>{
+    return await mongoose.connect("mongodb+srv://gym-app-b:gym-app-b@cluster0.cpp908y.mongodb.net/?retryWrites=true&w=majority")
+}
+module.exports= connect
